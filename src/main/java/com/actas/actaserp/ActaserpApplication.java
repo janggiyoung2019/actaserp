ccd3afb2da9b3bc5aa6a092209fa9d2e83b414da
actaserp <actas5200@gmail.com>
2021-12-23 20:08:05 +0900

    Initial commit

---
+++ src/main/java/com/actas/actaserp/ActaserpApplication.java
@@ -0,0 +1,13 @@
+package com.actas.actaserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActaserpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActaserpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actas/actaserp/ActaserpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actas.actaserp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActaserpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
